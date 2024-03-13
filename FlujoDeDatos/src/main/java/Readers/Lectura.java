dfd84be29fac60ca18f41840da63c3b29b17b972
guadalupe-rivarola <guadalupe.rivarola.et32@gmail.com>
2024-03-13 11:17:00 -0300

    configurar

---
+++ FlujoDeDatos/src/main/java/Readers/Lectura.java
@@ -0,0 +1,30 @@
+package Readers;
+
+
+import java.util.Scanner;
+
+public class Lectura {
+	// Stream
+	// Stream  IN
+	// Stream  OUT
+	// Stream  ERR
+	
+	public Lectura()
+	{
+		
+		System.out.println("Byte:");
+		//Scanner s = new Scanner(System.in);
+		//System.err.println();
+		
+		
+		
+		System.out.write(  13  );  // 
+		System.out.write(  10  );  // enter 
+		System.out.write(  5 );
+		System.out.write(  27 );
+		System.out.flush();
+		//System.out.println();
+	}
+	
+	
+}

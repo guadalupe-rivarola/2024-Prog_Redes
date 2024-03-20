75475294549c3b6351f96fc03bec2a2ba7cd38c5
guadalupe-rivarola <guadalupe.rivarola.et32@gmail.com>
2024-03-20 12:34:14 -0300

    entrada y lectura de datos

--- FlujoDeDatos/src/main/java/Readers/Lectura.java
+++ FlujoDeDatos/src/main/java/Readers/Lectura.java
@@ -2,6 +2,8 @@ package Readers;
 
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.PrintStream;
 import java.util.Scanner;
@@ -26,7 +28,7 @@ public class Lectura {
 		
 		//escribe byte a byte 
 		// ejemplo: el byte 27 no tiene representacion y se ve un ?
-		System.out.write(  13  );  // 
+		System.out.write(  104  );  // 
 		System.out.write(  10  );  // enter 
 		System.out.write(  5 );
 		System.out.write(  97 );
@@ -42,26 +44,70 @@ public class Lectura {
 		
 		System.err.println("ERROR");
 		
-		System.out.flush(); //asegura que la informacion en el canal salga, en este caso OUT
+		System.out.flush(); //asegura que la informacion en el canal de comunicacion salga
 		//System.out.println();
 		
 		ps.println("holaaa");
+		
 		// printf : permite armar el formato
 		//%d enteros
 		//%b boolean
 		//%s text
-		//%f numeros con coma
+		//%f numeros con como
 		//%.2f solo con 2 decimales
-		ps.printf("EL numero elegido es: %d y su nombre: %s y en su cuenta tiene: %.2f $", 18, "Guada", 9574.34);
+		
+		ps.printf("EL numero elegido es: %b %d y su nombre: %s y en su cuenta tiene: %.2f $ \n", true, 18, "Guada", 9574.34);
+		
+		
+		//System.set..() : cambian el canal de comunicacion por defecto.
+		//System.setErr(error);
+		//System.setOut(error);
+		//System.setIn(null);
 		
 		File archivo = new File("error.log");
 		
-		PrintStream error = new PrintStream();
+		//crea un canal de comunicaciones de SALIDA, destino:  un ARCHIVO (file)
+		
+		FileOutputStream fos = null;
+		try {
+			fos = new FileOutputStream(archivo , true);
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		PrintStream error = new PrintStream(fos);
+		
 		System.setErr(error);
 		
-		System.err.println("Eto es un eror critico");
+		
+		
+		System.err.println("Esto es un error critico");
 		
 	}
 	
+	public String entradaDeDatos() {
+		//lee los bytes
+		try {
+			
+			int Byte = -1;
+			String cadena = "";
+			while( (Byte = System.in.read()) != '\n'   ) {
+				
+				if ((Byte != 13)) 
+					cadena += (char)Byte;
+				
+			}
+			
+			System.out.print(cadena);
+		
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return "";
+		
+	}
 	
 }
--- FlujoDeDatos/src/main/java/Readers/main.java
+++ FlujoDeDatos/src/main/java/Readers/main.java
@@ -5,6 +5,8 @@ public class main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Lectura lec = new Lectura();
+		
+		lec.entradaDeDatos();
 	}
 
 }

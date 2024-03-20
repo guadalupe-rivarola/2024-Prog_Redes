2e8701d56d08ea37053712ac835bbae4363d378f
guadalupe-rivarola <guadalupe.rivarola.et32@gmail.com>
2024-03-20 12:46:46 -0300

    Update main.java
    string

--- FlujoDeDatos/src/main/java/Readers/main.java
+++ FlujoDeDatos/src/main/java/Readers/main.java
@@ -1,12 +1,27 @@
 package Readers;
 
+import java.io.PrintStream;
+
 public class main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Lectura lec = new Lectura();
+		//Lectura lec = new Lectura();
 		
-		lec.entradaDeDatos();
+		//lec.entradaDeDatos();
+		
+		//String leido = lec.entradaDeDatos();
+		
+		PrintStream ps = new PrintStream(System.out);
+		
+		String texto = "   Este va a ser el, texto <> que yo quiero, manipular";
+		
+		ps.println("El char 3:" + texto.charAt(3) );
+		ps.println("sin espacio:" + texto.trim() );
+		ps.println("el esta en la posicion :" + texto.indexOf("el") );
+		ps.println("el > esta en la posicion:" + texto.indexOf('<') );
+		
+		
 	}
 
 }

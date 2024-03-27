d8a973a54a5f0134b4064a7c511bfe80cb719a00
guadalupe-rivarola <guadalupe.rivarola.et32@gmail.com>
2024-03-27 12:39:11 -0300

    27/03
    27/03

--- FlujoDeDatos/src/main/java/Readers/main.java
+++ FlujoDeDatos/src/main/java/Readers/main.java
@@ -16,10 +16,59 @@ public class main {
 		
 		String texto = "   Este va a ser el, texto <> que yo quiero, manipular";
 		
-		ps.println("El char 3:" + texto.charAt(3) );
+		ps.println("ASCII el char 3:" + texto.charAt(3) );
 		ps.println("sin espacio:" + texto.trim() );
 		ps.println("el esta en la posicion :" + texto.indexOf("el") );
 		ps.println("el > esta en la posicion:" + texto.indexOf('<') );
+		
+		ps.println("ultima aparicion de la letra e" + texto.lastIndexOf('e'));
+		ps.println("largo del texto" + texto.length());
+		
+		//compara si tiene el texto y revisa si la variable es exactamente igual (objeto completo)
+		ps.println("" + texto.equals(""));
+		
+		//compara si tiene el texto (solo el contenido)
+		ps.println("" + texto.compareTo("hola"));
+		
+		//devuelve true o false si lo que escribí esta en el texto
+		ps.println("" + texto.contains("que"));
+		
+		//reemplaza solo pa primera aparicion de un caracter por otro
+		ps.println("" + texto.replace("a","4"));
+		
+		//reemplaza todas las  apariciones de un caracter por otro
+		ps.println("" + texto.replaceAll("a","4"));
+		
+		ps.println("" + texto.concat("otro texto").concat("otro texto"));
+		
+		ps.println("" + texto.toLowerCase());
+		ps.println("" + texto.toUpperCase());
+		
+		
+		ps.println("el texto empieza con Este:" + texto.startsWith("Este"));
+		ps.println("el texto termina con ." + texto.endsWith("."));
+		
+		//estraer una parte de un texto desde la posicion que escribo
+		ps.println("" + texto.substring(10));
+		
+		//apartir de la posicin que escribo primero, le escribo cuantas posiciones agarrar
+		ps.println("" + texto.substring(10,8));
+		
+		//estraer una parte de un texto desde la posicion que escribo hasta la posicion donde estan esos caracterres
+		ps.println("" + texto.substring(0, texto.indexOf("<>")));
+		
+		//retorna el caracter que esta en la posicion escrita en unicode 
+		ps.println("UNICODE (E): " + texto.codePointAt(4));
+		
+		//cadena de caracteres en vector de caracteres
+		char[] letraAletra = texto.toCharArray();
+		
+		//
+		String separar = "esto, es un texto, que vamos, a separar.";
+		String[] vec = separar.split(","); //["esto" , " es un texto" , " que vamos" , " a separar."]
+		separar.split("\\+"); // es como \n \+
+		texto.split(" <> ");
+		
 		
 		
 	}
